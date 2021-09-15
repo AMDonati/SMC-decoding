6b75bfa736c8fbd91e97da768645055e9ff1a88a
AMDonati <alice.martindonati@gmail.com>
2021-09-15 10:13:51 +0200

    sh

--- src/sh/slurm/decode_with_attribute_sst.sh
+++ src/sh/slurm/decode_with_attribute_sst.sh
@@ -5,8 +5,8 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8g
-#SBATCH --output=slurm_out/sst/sst_gpt2%j.out
-#SBATCH --error=slurm_out/sst/sst_gpt2%j.err
+#SBATCH --output=slurm_out/sst/dec-sst-%j.out
+#SBATCH --error=slurm_out/sst/dec-sst-%j.err
 #SBATCH --time=20:00:00
 #SBATCH -A ktz@gpu
 
